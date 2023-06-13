1d1ff1663cc97f104ee0d89331965297d9a16c89
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2023-06-13 20:11:43 +0500

    taskproject

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView,TaskIdView,TaskDoneView,TaskUndoneView
 
 urlpatterns = [
     path("tasks/", TaskListView.as_view()),
+    path('tasks/<int:id>',TaskIdView.as_view()),
+    path('tasks/<int:id>/done',TaskDoneView.as_view()),
+    path('tasks/<int:id>/undone',TaskUndoneView.as_view())
 ]
